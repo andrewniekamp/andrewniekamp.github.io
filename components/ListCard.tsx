36a25787fb7ea1e0010a1fd715a49d7df44ad754
Andrew Niekamp <aniekamp1@udayton.edu>
2023-04-30 19:49:38 -0400

    Add list card component

---
+++ components/ListCard.tsx
@@ -0,0 +1,28 @@
+interface Props {
+  icon: any;
+  header: string;
+  list: { icon: any; text: string }[];
+}
+
+export default function ListCard(props: Props) {
+  const { icon, header, list } = props;
+  return (
+    <div className="rounded-md overflow-hidden shadow-lg p-4 w-64">
+      <div className="px-6 py-4">
+        <div className="flex font-bold text-xl mb-2 gap-2">
+          {icon}
+          {header}
+        </div>
+        <ul className="list-none list-inside text-gray-700">
+          {list.map((item, i) => (
+            <li key={`item-${i}}`}>
+              <div className="flex gap-2">
+                {item.icon} {item.text}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
