5e322b75d2fe7b10dd43e292c4eb00d8512ed320
Andrew Niekamp <aniekamp1@udayton.edu>
2023-05-01 18:03:58 -0400

    Add socials to resume page

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,11 +1,16 @@
+import Socials from "@/components/Socials";
 import Timeline from "@/components/timeline/Timeline";
+import { EMAIL, GITHUB, LINKEDIN } from "@/utils/constants-index";
 import { RESUME_RECORDS } from "@/utils/constants-resume";
 
 export default function Resume() {
   return (
     <main className="flex flex-col justify-center items-center p-2 md:p-8">
       <p className="responsive-header max-w-screen text-center">Resume</p>
-      <div className="flex pt-12 pb-10">
+      <div className="pt-2">
+        <Socials email={EMAIL} gitHub={GITHUB} linkedIn={LINKEDIN} />
+      </div>
+      <div className="flex pt-5 pb-10">
         <Timeline accomplishments={RESUME_RECORDS} />
       </div>
     </main>
