627e34e4e46df1799c2cbbfa471bb84da6fde583
Andrew Niekamp <aniekamp1@udayton.edu>
2023-04-30 20:59:10 -0400

    Fix tab border

--- components/Tab.tsx
+++ components/Tab.tsx
@@ -14,9 +14,9 @@ export default function Tab(props: Props) {
         onClick={() => selectTab(position)}
         className={`${
           active
-            ? " active border-blue-500 dark:border-blue-500 dark:text-blue-500  text-blue-600"
-            : "hover:border-gray-300 dark:hover:text-gray-300 hover:text-gray-600 border-transparent"
-        } inline-block p-4 border-b-2 border-transparent rounded-t-lg  `}
+            ? " active border-b-2 border-blue-400  dark:border-blue-500 dark:text-blue-500  text-blue-600"
+            : " hover:border-gray-300 dark:hover:text-gray-300 hover:text-gray-600 border-transparent"
+        } inline-block p-4  border-transparent rounded-t-lg  `}
         aria-current={active}
       >
         {label}
