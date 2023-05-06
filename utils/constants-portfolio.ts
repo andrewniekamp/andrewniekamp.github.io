0736e3e9c3681b2c53c79dc3b4822194ab43b40d
Andrew Niekamp <aniekamp1@udayton.edu>
2023-05-06 11:11:13 -0400

    Add portfolio constants

---
+++ utils/constants-portfolio.ts
@@ -0,0 +1,44 @@
+import { Project } from "./types";
+
+export const projects: Project[] = [
+  {
+    title: "Workshop",
+    description: "A writers' community for facilitating workshops.",
+    imageUrl: "/images/portfolio/workshop/landing.png",
+    technologies: [
+      "TypeScript, JavaScript, Node.js, Next.js, Material UI, PostgreSQL",
+    ],
+    logoUrl: "/images/portfolio/workshop/logo-transparent.png",
+  },
+  {
+    title: "Video Feedback",
+    description: "An app for commenting on video submissions.",
+    imageUrl: "/images/portfolio/video-comments/student_review.png",
+    technologies: [""],
+  },
+  {
+    title: "Rudy's Adventure",
+    description: "A 2D retro rpg-style campus Easter egg hunt.",
+    imageUrl: "/images/portfolio/rudy/easter.png",
+    technologies: [""],
+  },
+  {
+    title: "Tokens",
+    description: "A novel way of managing excused absences and late work.",
+    imageUrl: "/images/portfolio/tokens/tokens_learner.png",
+    technologies: [""],
+  },
+  {
+    title: "Shoutouts",
+    description:
+      "An app for giving and receiving praise for exceptional contributions.",
+    imageUrl: "/images/portfolio/shoutouts/student_landing.png",
+    technologies: [""],
+  },
+  {
+    title: "Props",
+    description: "A task-based property management mobile app.",
+    imageUrl: "/images/portfolio/props/confirmation.png",
+    technologies: [""],
+  },
+];
